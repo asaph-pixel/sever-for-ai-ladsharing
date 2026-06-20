33d1ee104ccb9208e12456a5a64a52faf48d24f1
asaph-pixel <asaphodongo@gmail.com>
2026-06-21 01:11:45 +0300

    initial commit

--- static/config.js
+++ static/config.js
@@ -1,2 +1,1 @@
-window.ZEPHOST_API_BASE_URL =
-  window.ZEPHOST_API_BASE_URL || "https://sever-for-ai-ladsharing-1.onrender.com";
+window.ZEPHOST_API_BASE_URL = "https://sever-for-ai-ladsharing.onrender.com";
